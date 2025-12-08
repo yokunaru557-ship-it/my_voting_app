483c2b406e92f3d95742e5af431e0b2012fbf226
Anastasia62356 <take6emi@gmail.com>
2025-12-08 11:28:00 +0900

    Update 1_議題一覧.py

--- my_voting_app/pages/1_議題一覧.py
+++ my_voting_app/pages/1_議題一覧.py
@@ -36,11 +36,11 @@ st.divider()
 if "fg" not in st.session_state:
     st.session_state["fg"] = 0  # 0: 期限順, 1: 新着順
 # 2列に分けてボタンを配置
-col1, col2, col3, col4 = st.columns([0.30, 0.30, 0.20, 0.20])
-with col3:
+col1, col2, col3, col4 = st.columns([0.20, 0.20, 0.30, 0.30])
+with col1:
     if st.button("⏰ 期限順"):
         st.session_state.fg = 0
-with col4:
+with col2:
     if st.button("🆕 新しい順"):
         st.session_state.fg = 1
 # ---------------------------------------------------------
@@ -110,6 +110,7 @@ for index, topic in topics_df.iterrows():
                 counts = topic_votes["option"].value_counts()
                 for opt in options:
                     st.write(f"{opt}：{counts.get(opt, 0)} 票")
+
 
 
 
